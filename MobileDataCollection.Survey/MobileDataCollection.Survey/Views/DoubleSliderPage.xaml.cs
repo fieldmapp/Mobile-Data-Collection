6569adc89c408a19d4244276ca436b5b8b81dcc8
MayaKoehnen <maya.koehnen@uni-jena.de>
2019-05-25 16:46:20 +0200

    Try binding in DoubleSliderPage

--- MobileDataCollection.Survey/MobileDataCollection.Survey/Views/DoubleSliderPage.xaml.cs
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Views/DoubleSliderPage.xaml.cs
@@ -12,9 +12,15 @@ namespace MobileDataCollection.Survey.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DoubleSliderPage : ContentPage
 	{
+         public ObservableCollection<Question> Items = new ObservableCollection<Question>()
+        {
+            new Question(){Text="Schätzen Sie den Grad der Bedeckung des Bodens durch Pflanzen (A) und den Anteil grüner Pflanzenbestandteile (B). ein", PictureAdresses={"C:\Users\PC\source\repos\LayoutSlider\LayoutSlider\LayoutSlider\Bilder\bedeckung1.PNG"}, RightAnswers={1}, Id= SurveyMenuItemType.DoubleSlider}
+        };
+        
 		public DoubleSliderPage ()
 		{
 			InitializeComponent();
+            Header.ItemsSource = Items;
 		}
 		 void OnSliderAValueChanged(object sender, ValueChangedEventArgs args)
         {
