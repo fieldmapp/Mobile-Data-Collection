4bb7941e8a15dd9153c3b0a02a42abe371ec304b
MayaKoehnen <maya.koehnen@uni-jena.de>
2019-05-25 18:25:28 +0200

    Binding for DoubleSliderPage

---
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Models/QuestionDoubleSliderPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileDataCollection.Survey.Models
+{
+	public class QuestionDoubleSliderPage
+    {
+        public SurveyMenuItemType Id { get; set; }
+        public string Text { get; set; }
+        public string[] PictureAdresses { get; set; }
+        public int[] RightAnswers { get; set; }
+
+    }
+}
--- MobileDataCollection.Survey/MobileDataCollection.Survey/Views/DoubleSliderPage.xaml.cs
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Views/DoubleSliderPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MobileDataCollection.Survey.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +14,16 @@ namespace MobileDataCollection.Survey.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class DoubleSliderPage : ContentPage
 	{
-         public ObservableCollection<Question> Items = new ObservableCollection<Question>()
+        QuestionDoubleSliderPage Item = new QuestionDoubleSliderPage()
         {
-            new Question(){Text="Schätzen Sie den Grad der Bedeckung des Bodens durch Pflanzen (A) und den Anteil grüner Pflanzenbestandteile (B). ein", PictureAdresses={"C:\Users\PC\source\repos\LayoutSlider\LayoutSlider\LayoutSlider\Bilder\bedeckung1.PNG"}, RightAnswers={1}, Id= SurveyMenuItemType.DoubleSlider}
+            Text = "Schätzen Sie den Grad der Bedeckung des Bodens durch Pflanzen (A) und den Anteil grüner Pflanzenbestandteile (B). ein",
+            PictureAdresses = new[] { @"C:\Users\PC\source\repos\LayoutSlider\LayoutSlider\LayoutSlider\Bilder\bedeckung1.PNG" },
+            RightAnswers = new[] { 1 }
         };
         
 		public DoubleSliderPage ()
 		{
 			InitializeComponent();
-            Header.ItemsSource = Items;
 		}
 		 void OnSliderAValueChanged(object sender, ValueChangedEventArgs args)
         {
