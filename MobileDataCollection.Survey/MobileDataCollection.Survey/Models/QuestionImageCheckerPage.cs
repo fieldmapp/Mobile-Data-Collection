6852abd26587c3f927da36041b8421322214d339
Henn Wo <Henning.Woydt@gmail.com>
2019-05-26 11:58:37 +0200

    Created Class for ImageCheckerPage and changed Layout in ImageCheckerPage

---
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Models/QuestionImageCheckerPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileDataCollection.Survey.Models
+{
+    class QuestionImageCheckerPage
+    {
+        public int NumberOfPossibleAnswers { get; set; }
+        public int NumberOfCorrectAnswers { get; set; }
+        public bool[] StandardSolution { get; set; }
+        public bool[] GivenSolution { get; set; }
+        public bool[] CorrectGivenSolution { get; set; }
+        public string[] SourcesBigPictures { get; set; }
+        public string[] SourcesSmallPictures { get; set; }
+    }
+}
