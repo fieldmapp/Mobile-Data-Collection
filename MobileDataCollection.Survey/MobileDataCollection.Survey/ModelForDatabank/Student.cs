eade684a03726fcafaccd2eb4ffcb05b360ede6e
Henn Wo <Henning.Woydt@gmail.com>
2019-06-05 15:15:57 +0200

    Test-Datenbank created

---
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/ModelForDatabank/Student.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileDataCollection.Survey.ModelForDatabank
+{
+    class Student
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+}
