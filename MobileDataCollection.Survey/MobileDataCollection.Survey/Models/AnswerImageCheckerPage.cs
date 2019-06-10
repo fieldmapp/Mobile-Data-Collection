9926c754f662b5b419fb4147e167caf3b1e1b8ec
max.moebius <max.moebius@uni-jena.de>
2019-06-10 15:16:15 +0200

    Implement bindings in IntrospectionPage, DoubleSliderPage, ImageCheckerPage. Implement BindableObject in all Models

--- MobileDataCollection.Survey/MobileDataCollection.Survey/Models/AnswerImageCheckerPage.cs
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Models/AnswerImageCheckerPage.cs
@@ -2,35 +2,64 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MobileDataCollection.Survey.Models
 {
-    class AnswerImageCheckerPage
+    public class AnswerImageCheckerPage : BindableObject
     {
+        public static readonly BindableProperty InternIdProperty = BindableProperty.Create(nameof(InternId), typeof(int), typeof(AnswerImageCheckerPage), 0, BindingMode.OneWay);
+        public static readonly BindableProperty Image1SelectedProperty = BindableProperty.Create(nameof(Image1Selected), typeof(int), typeof(AnswerImageCheckerPage), 0, BindingMode.OneWay);
+        public static readonly BindableProperty Image2SelectedProperty = BindableProperty.Create(nameof(Image2Selected), typeof(int), typeof(AnswerImageCheckerPage), 0, BindingMode.OneWay);
+        public static readonly BindableProperty Image3SelectedProperty = BindableProperty.Create(nameof(Image3Selected), typeof(int), typeof(AnswerImageCheckerPage), 0, BindingMode.OneWay);
+        public static readonly BindableProperty Image4SelectedProperty = BindableProperty.Create(nameof(Image4Selected), typeof(int), typeof(AnswerImageCheckerPage), 0, BindingMode.OneWay);
+
+
         /// <summary>
         /// Intern Id for Answers of this Type, corrosponds to same number as in QuestionImageCheckerPage
         /// </summary>
         [PrimaryKey]
-        public int InternId { get; set; }
+        public int InternId
+        {
+            get => (int)GetValue(InternIdProperty);
+            set => SetValue(InternIdProperty, value);
+        }
+
         /// <summary>
         /// Reflects whether Image 1 is selected
         /// </summary>
-        public int Image1Selected { get; set; }
+        public int Image1Selected
+        {
+            get => (int)GetValue(Image1SelectedProperty);
+            set => SetValue(Image1SelectedProperty, value);
+        }
 
         /// <summary>
         /// Reflects whether Image 2 is selected
         /// </summary>
-        public int Image2Selected { get; set; }
+        public int Image2Selected
+        {
+            get => (int)GetValue(Image2SelectedProperty);
+            set => SetValue(Image2SelectedProperty, value);
+        }
 
         /// <summary>
         /// Reflects whether Image 3 is selected
         /// </summary>
-        public int Image3Selected { get; set; }
+        public int Image3Selected
+        {
+            get => (int)GetValue(Image3SelectedProperty);
+            set => SetValue(Image3SelectedProperty, value);
+        }
 
         /// <summary>
         /// Reflects whether Image 4 is selected
         /// </summary>
-        public int Image4Selected { get; set; }
+        public int Image4Selected
+        {
+            get => (int)GetValue(Image4SelectedProperty);
+            set => SetValue(Image4SelectedProperty, value);
+        }
 
         /// <summary>
         /// Is a reference to the belonging Question
@@ -43,11 +72,6 @@ namespace MobileDataCollection.Survey.Models
             Image2Selected = selected2;
             Image3Selected = selected3;
             Image4Selected = selected4;
-        }
-
-        public AnswerImageCheckerPage()
-        {
-            
         }
     }
 }
--- MobileDataCollection.Survey/MobileDataCollection.Survey/Models/AnswerIntrospetionPage.cs
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Models/AnswerIntrospetionPage.cs
@@ -1,24 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MobileDataCollection.Survey.Models
 {
-    class AnswerIntrospetionPage
+    public class AnswerIntrospetionPage : BindableObject
     {
+        public static readonly BindableProperty SelectedAnswerProperty = BindableProperty.Create(nameof(SelectedAnswer), typeof(int), typeof(AnswerIntrospetionPage), 0, BindingMode.OneWay);
+        public static readonly BindableProperty QuestionProperty = BindableProperty.Create(nameof(Question), typeof(QuestionIntrospectionPage), typeof(AnswerIntrospetionPage), null, BindingMode.OneWay);
+
         /// <summary>
-        /// Represents the index of the selected Answer (in inclusive range 1 - 4)
+        /// Represents the index of the selected Answer (in inclusive range 1 - 5)
         /// </summary>
-        public int SelectedAnswer { get; set; }
+        public int SelectedAnswer
+        {
+            get => (int)GetValue(SelectedAnswerProperty);
+            set => SetValue(SelectedAnswerProperty, value);
+        }
 
         /// <summary>
         /// Is a reference to the belonging Question
         /// </summary>
-        public QuestionIntrospectionPage Question { get; set; }
+        public QuestionIntrospectionPage Question
+        {
+            get => (QuestionIntrospectionPage)GetValue(QuestionProperty);
+            set => SetValue(QuestionProperty, value);
+        }
 
-        public AnswerIntrospetionPage(QuestionIntrospectionPage question)
+        public AnswerIntrospetionPage(QuestionIntrospectionPage question, int selectedanswer)
         {
             Question = question;
+            SelectedAnswer = selectedanswer;
         }
     }
 }
