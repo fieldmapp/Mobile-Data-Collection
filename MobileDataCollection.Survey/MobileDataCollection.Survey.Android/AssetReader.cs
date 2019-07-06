65fc7f3a9f5e5f997177f5eb0005dc6bf9279b9c
Henn Wo <Henning.Woydt@gmail.com>
2019-07-06 13:35:53 +0200

    Added an Asset Reader for reading the txt

---
+++ MobileDataCollection.Survey/MobileDataCollection.Survey.Android/AssetReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace MobileDataCollection.Survey.Droid
+{
+    class AssetReader
+    {
+        public String readAssetTxt(string sourceTxt)
+        {
+            AssetManager assetManager = Forms.Context.Assets;
+            String Text;
+            using (StreamReader sr = new StreamReader(assetManager.Open("ImageCheckerQuestions.txt")))
+            {
+                Text = sr.ReadToEnd();
+            }
+            return Text;
+        }
+    }
+}
