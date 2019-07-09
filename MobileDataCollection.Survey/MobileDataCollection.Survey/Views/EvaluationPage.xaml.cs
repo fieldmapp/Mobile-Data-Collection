d287136ae826172bf3cd09682ac4802fe9ef1862
BMI24 <mitgliedschaftfo@gmx.de>
2019-07-09 02:01:37 +0200

    Revert "Apply Bindings in EvaluationPage"
    This reverts commit 4de4094b14b638f944b2503f82eae828a1482605.

--- MobileDataCollection.Survey/MobileDataCollection.Survey/Views/EvaluationPage.xaml.cs
+++ MobileDataCollection.Survey/MobileDataCollection.Survey/Views/EvaluationPage.xaml.cs
@@ -13,21 +13,49 @@ namespace MobileDataCollection.Survey.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class EvaluationPage : ContentPage
     {
-        public static readonly BindableProperty EvaluationItemProperty = BindableProperty.Create(nameof(EvaluationItem),
-            typeof(EvaluationItem), typeof(EvaluationPage), null);
+        public static readonly BindableProperty PercentLabelProperty = BindableProperty.Create(nameof(PercentLabelText),
+            typeof(String), typeof(EvaluationPage), "100%", BindingMode.OneWay);
 
-        private EvaluationItem EvaluationItem
+        public String PercentLabelText
         {
-            get => (EvaluationItem)GetValue(EvaluationItemProperty);
-            set => SetValue(EvaluationItemProperty, value);
+            get { return (String)GetValue(PercentLabelProperty); }
+            set { SetValue(PercentLabelProperty, value); }
         }
+
+        public static readonly BindableProperty PercentProperty = BindableProperty.Create(nameof(PercentBarValue),
+            typeof(double), typeof(EvaluationPage), 0.2, BindingMode.OneWay);
+        public double PercentBarValue
+        {
+            get { return (double)GetValue(PercentProperty); }
+            set { SetValue(PercentProperty, value); }
+        }
+
+        /* public static readonly BindableProperty BarColorProperty = BindableProperty.Create(nameof(BarColor),
+            typeof(Color), typeof(EvaluationPage), Color.DarkSeaGreen, BindingMode.OneWay);
+       public Color BarColor
+        {
+            get { return (Color)GetValue(BarColorProperty); }
+            set { SetValue(BarColorProperty, value); }
+        }*/
+        public static readonly BindableProperty ProgressColorProperty = BindableProperty.Create(nameof(ProgressColor),
+            typeof(Color), typeof(EvaluationPage), Color.DarkOliveGreen, BindingMode.OneWay);
+        public Color ProgressColor
+        {
+            get { return (Color)GetValue(ProgressColorProperty); }
+            set { SetValue(ProgressColorProperty, value); }
+        }
+
+        private EvaluationItem EvaluationItem;
 
         public EvaluationPage(EvaluationItem evalItem)
         {
             InitializeComponent();
             EvaluationItem = evalItem;
+            PercentBarValue = (double)evalItem.Percent / 100;
             PercentBar.BindingContext = this;
+            ProgressColor = evalItem.BarColor;
             PercentLabel.BindingContext = this;
+            PercentLabelText = $"{evalItem.Percent}%";
         }
         void DetailsClicked(object sender, EventArgs e)
         {
