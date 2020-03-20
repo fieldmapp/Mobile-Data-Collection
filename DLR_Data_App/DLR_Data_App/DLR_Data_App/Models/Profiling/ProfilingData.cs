fff109900b004f3f7f630be2b9e9f0505bbf3f8c
BMI24 <mitgliedschaftfo@gmx.de>
2020-03-20 17:20:54 +0100

    Add JsonTranslator, providing two methods to translate an object to and from json

--- DLR_Data_App/DLR_Data_App/DLR_Data_App/Models/Profiling/ProfilingData.cs
+++ DLR_Data_App/DLR_Data_App/DLR_Data_App/Models/Profiling/ProfilingData.cs
@@ -10,21 +10,6 @@ namespace DLR_Data_App.Models.Profiling
 {
     public class ProfilingData
     {
-        /// <summary>
-        /// Shared JsonSerializer. Is threadsafe, see https://www.newtonsoft.com/json/help/html/JsonNetVsDotNetSerializers.htm
-        /// </summary>
-        static JsonSerializer JsonSerializer;
-
-        static ProfilingData()
-        {
-            JsonSerializer = new JsonSerializer
-            {
-                TypeNameHandling = TypeNameHandling.Auto,
-                ContractResolver = IgnoreBindableObjectContractResolver.Instance,
-                Formatting = Formatting.Indented,
-                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
-            };
-        }
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
@@ -50,21 +35,8 @@ namespace DLR_Data_App.Models.Profiling
         [JsonIgnore]
         public string ProfilingMenuItemsJson
         {
-            get
-            {
-                using (var stringWriter = new StringWriter())
-                using (var jsonWriter = new JsonTextWriter(stringWriter))
-                {
-                    JsonSerializer.Serialize(jsonWriter, ProfilingMenuItems);
-                    return stringWriter.ToString();
-                }
-            }
-            set
-            {
-                using (var stringReader = new StringReader(value))
-                using (var jsonReader = new JsonTextReader(stringReader))
-                    ProfilingMenuItems = JsonSerializer.Deserialize<List<ProfilingMenuItem>>(jsonReader);
-            }
+            get => JsonTranslator.GetJson(ProfilingMenuItems);
+            set => ProfilingMenuItems = JsonTranslator.GetFromJson<List<ProfilingMenuItem>>(value);
         }
 
         [SQLite.Ignore]
---
+++ DLR_Data_App/DLR_Data_App/DLR_Data_App/Services/JsonTranslator.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DLR_Data_App.Services
+{
+    static class JsonTranslator
+    {
+        /// <summary>
+        /// Shared JsonSerializer. Is threadsafe, see https://www.newtonsoft.com/json/help/html/JsonNetVsDotNetSerializers.htm
+        /// </summary>
+        static JsonSerializer JsonSerializer;
+
+        static JsonTranslator()
+        {
+            JsonSerializer = new JsonSerializer
+            {
+                TypeNameHandling = TypeNameHandling.Auto,
+                ContractResolver = IgnoreBindableObjectContractResolver.Instance,
+                Formatting = Formatting.Indented,
+                TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+            };
+        }
+
+        public static string GetJson(object input)
+        {
+            using (var stringWriter = new StringWriter())
+            using (var jsonWriter = new JsonTextWriter(stringWriter))
+            {
+                JsonSerializer.Serialize(jsonWriter, input);
+                return stringWriter.ToString();
+            }
+        }
+
+        public static T GetFromJson<T>(string json)
+        {
+            using (var stringReader = new StringReader(json))
+            using (var jsonReader = new JsonTextReader(stringReader))
+                return JsonSerializer.Deserialize<T>(jsonReader);
+        }
+    }
+}
