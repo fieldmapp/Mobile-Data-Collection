917f3b94f8c9186e27fe7140af8118cee0d37707
BMI24 <maximilian.enderling@uni-jena.de>
2020-06-19 15:11:04 +0200

    Replace text input by InlinePicker in LoginPage

--- DLR_Data_App/DLR_Data_App/DLR_Data_App/Views/Login/LoginPage.xaml.cs
+++ DLR_Data_App/DLR_Data_App/DLR_Data_App/Views/Login/LoginPage.xaml.cs
@@ -4,21 +4,35 @@ using System;
 using DLR_Data_App.ViewModels.Login;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+using DLR_Data_App.Models.Profiling;
+using System.Linq;
 
 namespace DLR_Data_App.Views.Login
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage
     {
+        public class UserDisplay : BindableObject, IInlinePickerElement
+        {
+            public static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(nameof(BackgroundColor), typeof(Color), typeof(UserDisplay), Color.Transparent, BindingMode.OneWay);
+            public Color BackgroundColor
+            {
+                get => (Color)GetValue(BackgroundColorProperty);
+                set => SetValue(BackgroundColorProperty, value);
+            }
+            public string Name { get; set; }
+        }
+
         private readonly LoginViewModel _viewModel = new LoginViewModel();
+        private List<UserDisplay> UserList;
         
         public LoginPage()
         {
+            UserList = Database.ReadUsers().Select(u => new UserDisplay { Name = u.Username }).ToList();
             InitializeComponent();
+            UserPicker.ItemSource = UserList;
             BindingContext = _viewModel;
-
-            // after completing the input of username change focus to login button
-            EntryUsername.Completed += (s, e) => BtnSignin.Focus();
         }
 
         /// <summary>
@@ -26,9 +40,9 @@ namespace DLR_Data_App.Views.Login
         /// </summary>
         private async void Btn_signin_Clicked(object sender, EventArgs e)
         {
-            var checkUsername = EntryUsername.Text;
+            var selectedUserName = ((UserDisplay)UserPicker.SelectedItem).Name;
 
-            if (_viewModel.Check_Information(checkUsername))
+            if (_viewModel.Check_Information(selectedUserName))
             {
                 Application.Current.MainPage = new SplashScreenPage();
             }
