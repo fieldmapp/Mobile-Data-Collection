e455437c1ee2ac54aee63614c092658754231d0f
BMI24 <maximilian.enderling@uni-jena.de>
2020-09-15 22:27:18 +0200

    Mostly finish ProjectOutputProcessor

---
+++ DLR_Data_App/ProjectOutputProcessor/Helpers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectOutputProcessor
+{
+    internal static class Helpers
+    {
+        public static IEnumerable<int> AllIndexesOf(this string str, string value)
+        {
+            // from https://stackoverflow.com/a/2641383
+
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentException("the string to find may not be empty", "value");
+            for (int index = 0; ; index += value.Length)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    break;
+                yield return index;
+            }
+        }
+        public static IEnumerable<int> AllIndexesOf(this string str, char value)
+        {
+            for (int index = 0; ; index += 1)
+            {
+                index = str.IndexOf(value, index);
+                if (index == -1)
+                    break;
+                yield return index;
+            }
+        }
+
+        public static int GetOriginalLengthInBytes(ReadOnlySpan<char> base64string)
+        {
+            // altered from https://blog.aaronlenoir.com/2017/11/10/get-original-length-from-base-64-string/ to work with spans
+            if (base64string.Length == 0)
+                return 0;
+
+            var characterCount = base64string.Length;
+            int paddingCount = 0;
+            if (base64string[^1] == '=')
+                paddingCount++;
+            if (base64string[^2] == '=')
+                paddingCount++;
+
+            return (3 * (characterCount / 4)) - paddingCount;
+        }
+    }
+}
--- DLR_Data_App/ProjectOutputProcessor/Program.cs
+++ DLR_Data_App/ProjectOutputProcessor/Program.cs
@@ -1,28 +1,93 @@
 ﻿using ExifLib;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectOutputProcessor
 {
     class Program
     {
+        const int MinBase64Length = 200;
         const string WorkingFileName = "current.jpg";
+
+        static bool DecodeBase64(string s, int start, int length, out byte[] result)
+        {
+            var base64Span = s.AsSpan(start, length);
+            result = new byte[Helpers.GetOriginalLengthInBytes(base64Span)];
+            return Convert.TryFromBase64Chars(base64Span, result, out int _);
+        }
+
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string imageBase64 = (await File.ReadAllLinesAsync("base64.txt"))[0];
+            string[] databaseOutput = await File.ReadAllLinesAsync("input.txt");
 
-            await File.WriteAllBytesAsync(@"current.jpg", Convert.FromBase64String(imageBase64));
-
-            bool dateFound = false;
-            DateTime datePictureTaken;
-            using (ExifReader reader = new ExifReader(WorkingFileName))
+            foreach (var lineWithIndex in databaseOutput.Select((l,i) => new { Value = l, Index = i }))
             {
-                dateFound = reader.GetTagValue(ExifTags.DateTimeDigitized, out datePictureTaken);
+                var indices = lineWithIndex.Value.AllIndexesOf('"').ToList();
+                if (indices.Count == 0)
+                    continue;
+
+                if (indices.Count % 2 == 1)
+                    throw new Exception("Malformed json file");
+
+                List<ReadOnlyMemory<char>> line_parts_reversed = new List<ReadOnlyMemory<char>>();
+                line_parts_reversed.Add(lineWithIndex.Value.AsMemory(indices[^1]));
+
+                for (int i = indices.Count - 2; i >= 0; i -= 2)
+                {
+                    var stringLength = indices[i + 1] - indices[i] - 1;
+                    if (stringLength >= MinBase64Length)
+                    {
+                        if (DecodeBase64(lineWithIndex.Value, indices[i] + 1, stringLength, out byte[] result))
+                        {
+                            await File.WriteAllBytesAsync(WorkingFileName, result);
+                            bool dateFound = false;
+                            DateTime datePictureTaken;
+                            using (ExifReader reader = new ExifReader(WorkingFileName))
+                            {
+                                dateFound = reader.GetTagValue(ExifTags.DateTimeDigitized, out datePictureTaken);
+                            }
+                            if (dateFound)
+                            {
+                                var file_name = $"img_{datePictureTaken:yyyyMMdd_HHmmss}.jpg";
+                                line_parts_reversed.Add(file_name.AsMemory());
+                                line_parts_reversed.Add("\"".AsMemory());
+                                File.Move(WorkingFileName, file_name, true);
+                            }
+                            else
+                            {
+                                File.Delete(WorkingFileName);
+                                line_parts_reversed.Add($"{'"'}removed_malformed_file".AsMemory());
+                            }
+                        }
+                        else
+                        {
+                            line_parts_reversed.Add(lineWithIndex.Value.AsMemory(indices[i], stringLength + 1));
+                        }
+                    }
+                    else
+                    {
+                        line_parts_reversed.Add(lineWithIndex.Value.AsMemory(indices[i], stringLength + 1));
+                    }
+
+                    //enter part between two strings
+                    int endIndexOfNextLeftPart = i == 0 ? 0 : indices[i - 1];
+                    int length = indices[i] - endIndexOfNextLeftPart;
+                    line_parts_reversed.Add(lineWithIndex.Value.AsMemory(endIndexOfNextLeftPart, length));
+                }
+
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = line_parts_reversed.Count - 1; i >= 0; i--)
+                {
+                    sb.Append(line_parts_reversed[i]);
+                }
+                databaseOutput[lineWithIndex.Index] = sb.ToString();
             }
-            if (dateFound)
-                File.Move(WorkingFileName, $"img_{datePictureTaken:yyyyMMdd_HHmmss}.jpg");
+            File.WriteAllLines("output.txt", databaseOutput);
         }
     }
 }
