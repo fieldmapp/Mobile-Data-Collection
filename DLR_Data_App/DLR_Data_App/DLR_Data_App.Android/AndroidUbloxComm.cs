44d855d7d9350182f683661f2b3ed8c146b4df6b
BMI24 <maximilian.enderling@uni-jena.de>
2020-10-28 13:10:19 +0100

    Configure ublox f9p at startup (disable nmea, enable some rtcm output messages)

--- DLR_Data_App/DLR_Data_App/DLR_Data_App.Android/AndroidUbloxComm.cs
+++ DLR_Data_App/DLR_Data_App/DLR_Data_App.Android/AndroidUbloxComm.cs
@@ -77,7 +77,11 @@ namespace com.DLR.DLR_Data_App.Droid
 
         class UsbBroadcastReciever : BroadcastReceiver
         {
+            const int READ_WAIT_MILLIS = 200;
+            const int WRITE_WAIT_MILLIS = 200;
+
             AndroidUbloxComm AndroidUbloxComm;
+            IUsbSerialPort SerialPort;
             SerialInputOutputManager SerialIOManager;
 
             public UsbBroadcastReciever(AndroidUbloxComm androidUbloxComm)
@@ -103,8 +107,8 @@ namespace com.DLR.DLR_Data_App.Droid
 
             public void OnHasUsbPermission()
             {
-                var port = AndroidUbloxComm.UbloxDriver.Ports.Single();
-                SerialIOManager = new SerialInputOutputManager(port)
+                SerialPort = AndroidUbloxComm.UbloxDriver.Ports.Single();
+                SerialIOManager = new SerialInputOutputManager(SerialPort)
                 {
                     BaudRate = 38400,
                     DataBits = 8,
@@ -114,11 +118,156 @@ namespace com.DLR.DLR_Data_App.Droid
                
                 SerialIOManager.DataReceived += (a, b) =>
                 {
-                    var message = UTF8Encoding.UTF8.GetString(b.Data);
+                    var message = BitConverter.ToString(b.Data);
                     Log.Verbose("ublox", message);
                 };
                 SerialIOManager.Open(AndroidUbloxComm.UsbManager);
+                WriteData(UbloxConfigurationMessageGenerator.StardardUbloxConfiguration());
             }
+
+            void WriteData(byte[] data)
+            {
+                if (SerialIOManager?.IsOpen == true)
+                {
+                    SerialPort.Write(data, WRITE_WAIT_MILLIS);
+                }
+            }
+        }
+
+        static class UbloxConfigurationMessageGenerator
+        {
+            // see https://www.u-blox.com/en/docs/UBX-18010854 3 UBX protocol (uses little endian)
+
+            static byte[] GenerateConfigurationItem(int key, byte[] value)
+            {
+                var result = new List<byte>(sizeof(int) + value.Length);
+                result.AddRange(GetAsLittleEndian(BitConverter.GetBytes(key)));
+                result.AddRange(GetAsLittleEndian(value));
+                return result.ToArray();
+            }
+
+            public static byte[] StardardUbloxConfiguration()
+            {
+                // see https://www.u-blox.com/en/docs/UBX-18010854 5.9.11 CFG-MSGOUT for message ids
+                // see https://www.use-snip.com/kb/knowledge-base/rtcm-3-message-list/ and https://www.use-snip.com/kb/knowledge-base/an-rtcm-message-cheat-sheet/ for message description
+                const byte rate = 1;
+                byte[] rateValue = new[] { rate };
+                byte[] falseValue = new byte[] { 0 };
+                byte[] trueValue = new byte[] { 0xFF };
+
+                // Stationary RTK Reference Station ARP
+                const int type1005UsbOutputRate = 0x209102c0;
+
+                // GPS  (1077 is the best to use.  Setting up uBlox with RTKLIB? – use this)
+                const int type1074UsbOutputRate = 0x20910361;
+                const int type1077UsbOutputRate = 0x209102cf;
+
+                // GLONASS  (and 1087 would be the one to use here)
+                const int type1084UsbOutputRate = 0x20910366;
+                const int type1087UsbOutputRate = 0x209102d4;
+
+                // Galileo
+                const int type1094UsbOutputRate = 0x2091036b;
+                const int type1097UsbOutputRate = 0x2091031b;
+
+                // BeiDou
+                const int type1124UsbOutputRate = 0x20910370;
+                const int type1127UsbOutputRate = 0x209102d9;
+
+                // GLONASS L1 and L2 Code-Phase Biases
+                const int type1230UsbOutputRate = 0x20910306;
+
+                // ublox proprietary ??
+                const int type4072_0UsbOutputRate = 0x20910301;
+                const int type4072_1UsbOutputRate = 0x20910384;
+
+                // See https://www.u-blox.com/en/docs/UBX-18010854 5.9.36 CFG - USBOUTPROT
+                const int usbOutputProtocolNmea = 0x10780002;
+                const int usbOutputProtocolUbx = 0x10780001;
+
+                return SetConfigurationItems(
+                    GenerateConfigurationItem(type1077UsbOutputRate, rateValue),
+                    GenerateConfigurationItem(type1087UsbOutputRate, rateValue),
+                    GenerateConfigurationItem(type1097UsbOutputRate, rateValue),
+                    GenerateConfigurationItem(type1127UsbOutputRate, rateValue),
+                    GenerateConfigurationItem(type1230UsbOutputRate, rateValue),
+                    GenerateConfigurationItem(usbOutputProtocolNmea, falseValue)
+                    );
+            }
+
+            static byte[] SetConfigurationItems(params byte[][] configurationItems)
+            {
+                // see https://www.u-blox.com/en/docs/UBX-18010854 3.10.26 UBX-CFG-VALSET
+
+                const int maxConfigurationItemCount = 64;
+                if (configurationItems.Length > maxConfigurationItemCount)
+                    throw new ArgumentException($"No more than {maxConfigurationItemCount} can be set in one operation");
+
+                const byte messageClass = 0x06;
+                const byte messageId = 0x8a;
+
+                const byte version = 0;
+                const byte applyOnlyToRamLayer = 0b1;
+                const byte reservedFiller0 = 0;
+                const byte reservedFiller1 = 0;
+
+                var payload = new List<byte> { version, applyOnlyToRamLayer, reservedFiller0, reservedFiller1 };
+
+                foreach (var configurationItem in configurationItems)
+                {
+                    payload.AddRange(configurationItem);
+                }
+
+                return CreateUbloxWrapper(messageClass, messageId, payload);
+            }
+
+            /// <summary>
+            /// Creates the wrapped message out of the supplied payload
+            /// </summary>
+            static byte[] CreateUbloxWrapper(byte messageClass, byte messageId, IEnumerable<byte> payload)
+            {
+                // steps:
+                // 1. m = class + id + length of payload + payload
+
+                var payloadSize = payload.Count();
+
+                if (payloadSize > ushort.MaxValue)
+                    throw new ArgumentException("Argument may not have more then ushort.max elements.");
+
+                byte[] payloadLengthBytes = BitConverter.GetBytes((ushort)payloadSize);
+
+                List<byte> message = new List<byte>(8 + payloadSize) { messageClass, messageId };
+                message.AddRange(GetAsLittleEndian(payloadLengthBytes));
+
+                message.AddRange(payload);
+
+                // 2. m = m + ubx checksum of m (see https://www.u-blox.com/en/docs/UBX-18010854) 3.4 UBX checksum )
+
+                byte checksum_a = 0;
+                byte checksum_b = 0;
+
+                for (int i = 0; i < message.Count; i++)
+                {
+                    // arithmetic overflow should be ignored
+                    checksum_a += message[i];
+                    checksum_b += checksum_a;
+                }
+
+                message.Add(checksum_a);
+                message.Add(checksum_b);
+
+
+                // 3. m = preamble + m
+
+                const byte Preamble0 = 0xb5;
+                const byte Preamble1 = 0x62;
+
+                message.InsertRange(0, new[] { Preamble0, Preamble1 });
+
+                return message.ToArray();
+            }
+
+            static IEnumerable<byte> GetAsLittleEndian(IEnumerable<byte> input) => BitConverter.IsLittleEndian ? input : input.Reverse();
         }
     }
 }
