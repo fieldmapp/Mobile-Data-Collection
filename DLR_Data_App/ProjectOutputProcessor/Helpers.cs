f7064fc0f28afd06d9dd340d6fd6fab269218bbb
BMI24 <maximilian.enderling@uni-jena.de>
2021-11-23 16:23:07 +0100

    Reuse ProjectOutputProcessor as FieldCartographerProcessor

--- DLR_Data_App/ProjectOutputProcessor/Helpers.cs
+++ DLR_Data_App/ProjectOutputProcessor/Helpers.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ProjectOutputProcessor
+namespace FieldCartographerProcessor
 {
     internal static class Helpers
     {
--- DLR_Data_App/ProjectOutputProcessor/Program.cs
+++ DLR_Data_App/ProjectOutputProcessor/Program.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectOutputProcessor
+namespace FieldCartographerProcessor
 {
     public class Options
     {
