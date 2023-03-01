0319ec35dfebb12bdc5d69500801581382ccc466
EricKrg1 <eric.krueger@dlr.de>
2023-03-01 12:08:42 +0100

    update line creator

--- DrivingViewProcessor/linestring_creator.py
+++ DrivingViewProcessor/linestring_creator.py
@@ -1,62 +1,79 @@
-from utils import read_ublox_pos, read_interaction, add_time_col, gpst_leapseconds, gpst_to_utc
+import geojson
 import pandas as pd
 import argparse
-from typing import Dict
-import geojson
+import geopandas as gpd
+from utils import read_ublox_pos, read_interaction, add_time_col, gpst_leapseconds, LaneSegment, LaneDetail
+from typing import Dict, List
+from geojson import FeatureCollection
+from itertools import chain
 
 parser = argparse.ArgumentParser(description='Read and Process interaction und ublox data.')
 parser.add_argument("--pos", type=str, nargs=1)
 parser.add_argument("--ilog", type=str, nargs=1)
+parser.add_argument("--filename", type=str, nargs=1)
 
 _merge_rcol = "UTC_corrected"
 _merge_lcol = "UTC_Seconds"
 _position_time = "GPST_Seconds"
+_lane_config = {
+    1: 5,
+    2: 10,
+    3: 15,
+    4: -5,
+    5: -10,
+    6: -15,
+}
+
+
+def lanes_from_df(df: gpd.GeoDataFrame, lane_config: dict) -> dict:
+    all_lanes: Dict[int, List[LaneSegment]] = dict()
+    for k, v in lane_config.items():
+        interactions_filtered: pd.DataFrame = df.loc[df["LaneIndex"] == k]
+        segment = None
+        lanes: [LaneSegment] = []
+        for interaction in interactions_filtered.itertuples(False):
+            detail = LaneDetail.from_pd(interaction)
+            if detail.ltype == 'open':
+                segment = LaneSegment(lane_index=interaction.LaneIndex, start=detail.utc_time)
+                segment.actions = [detail]
+                lanes.append(segment)
+            elif detail.ltype == 'close':
+                segment.end = detail.utc_time
+                segment.actions.append(detail)
+            else:
+                segment.actions.append(detail)
+        all_lanes[k] = lanes
+    return all_lanes
+
+
+def main(positions_df: pd.DataFrame, interactions_df: pd.DataFrame, file_name: str):
+    # add time columns, atm. both ways of calc.
+    add_time_col(time_func=gpst_leapseconds, df=positions_df, col_name=_merge_rcol, base_col=_position_time)
+    interactions_df = interactions_df.sort_values(by=_merge_lcol)  # only needed for the test data set
+    # merge interactions_df and position
+    interactions_df = pd.merge_asof(interactions_df, positions_df, left_on=_merge_lcol, right_on=_merge_rcol)
+    # is the geopandas df really nes. here?
+    geometry = gpd.points_from_xy(interactions_df["lat_deg"], interactions_df['long_deg'], crs="EPSG:4326")
+    interactions_df_gdf = gpd.GeoDataFrame(data=interactions_df, geometry=geometry)
+    # collect all lanes
+    result_lanes = lanes_from_df(interactions_df_gdf, _lane_config)
+    # lanes to geojson
+    features = [[e.to_geojson_feature(_lane_config) for e in v] for k, v in result_lanes.items()]
+    feature_list = list(chain.from_iterable(features))
+    f_list = [e for l in feature_list for e in l]  # unpack tuples
+    collection = FeatureCollection(features=f_list)
+
+    # write to file
+    out = geojson.dumps(collection, default=lambda x: x.__dict__)
+    with open(file_name, 'w') as f:
+        f.write(out)
+    print("done")
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    print(args)
-
     positions = read_ublox_pos(args.pos[0])
     interactions = read_interaction(args.ilog[0])
-    # add time columns, atm. both ways of calc.
-    add_time_col(time_func=gpst_leapseconds, df=positions, col_name=_merge_rcol, base_col=_position_time)
-    add_time_col(time_func=gpst_to_utc, df=positions, col_name=_merge_lcol, base_col=_position_time)
-    # merge interactions and position
-    interactions_new = pd.merge_asof(interactions, positions, on=_merge_lcol)
-    interactions_alt = pd.merge_asof(interactions, positions, left_on=_merge_lcol, right_on=_merge_rcol)
-    assert(interactions.shape[0] == interactions.shape[0])
-    """
+    file_name = args.filename[0]
+    main(positions, interactions, file_name)
 
-
-    class LaneData():
-        start: geojson.Point
-        end: geojson.Point
-        cause: str
-        type: str
-
-
-    lanes: Dict[int, LaneData] = dict()
-
-    for interaction in interactions.itertuples(False):
-        if interaction.Action.startswith('Miss'):
-            continue
-        print(interaction)
-        if interaction.Action == 'canceled':
-            lanes = dict()
-        elif interaction.Action == 'open':
-            if interaction.LaneIndex in lanes:
-                # finalize lane
-                pass
-
-            lanes[interaction.LaneIndex] = LaneData()
-            lanes[interaction.LaneIndex].start = interaction.
-        elif interaction.Action == 'close':
-            pass
-        elif interaction.Action.startswith('damage='):
-            pass
-        elif interaction.Action.startswith('cause='):
-            pass
-        else:
-            raise NotImplementedError(
-                f'Unexpected interaction action encountered: {interaction.Action} at lane {interaction.LaneIndex}')
-        """
--- DrivingViewProcessor/utils.py
+++ DrivingViewProcessor/utils.py
@@ -1,7 +1,83 @@
-import datetime
-
 import pandas as pd
 from astropy import time
+from geojson import Feature, MultiPoint, MultiLineString, LineString
+import math
+import shapely.geometry as shp
+
+
+class LaneDetail:
+    utc_time: int
+    lat: int
+    lon: int
+    ltype: str
+    geom: shp.Point
+
+    def __init__(self, utc_time, lat, lon, ltype):
+        self.utc_time = utc_time
+        self.lat = lat
+        self.lon = lon
+        self.ltype = ltype
+
+    @classmethod
+    def from_pd(cls, pandas):
+        return LaneDetail(pandas.UTC_Seconds, pandas.geometry.x, pandas.geometry.y, pandas.Action)
+
+
+class LaneSegment:
+    start: int
+    end: int
+    actions: [LaneDetail]
+    lane: int
+
+    def __init__(self, lane_index: int, start: int):
+        self.lane = lane_index
+        self.start = start
+        self.actions = []
+
+    def to_props(self):
+        res = self.__dict__
+        res = self.get_cause(res)
+        return res
+
+    def get_cause(self, res: dict):
+        for i in self.actions:
+            if "=" in i.ltype:
+                split = i.ltype.split("=")
+                res[split[0]] = split[1]
+        return res
+
+    def to_geojson_feature(self, lane_config: dict) -> Feature:
+        """
+        :param lane_config: Configuration of the field lanes: number and width
+        :return: GeoJson Feature with the calculated parallel Lines representing the FieldLanes
+                as well as a Multipoint feature representing the GPS-Track
+        """
+        open_node = next(x for x in self.actions if x.ltype == "open")
+        close_node = next(x for x in self.actions if x.ltype == "close")
+
+        if open_node is None:
+            raise ValueError(f"No open node found for {self}")
+        if close_node is None:
+            raise ValueError(f"No close node found for {self}")
+
+        a, b = calc_parallel_c((open_node.lon, open_node.lat), (close_node.lon, close_node.lat),
+                               meters_to_degrees(lane_config[self.lane], (open_node.lat+close_node.lat)/2)[1])
+        # constructed parallel shifted line
+        line_shifted = LineString([a, b], precision=15)
+        raw_points = MultiPoint([(open_node.lon, open_node.lat), (close_node.lon, close_node.lat)], precision=15)
+
+        multi_lines = MultiLineString([line_shifted], precision=15)
+        line_prop = {
+            "start": self.start,
+            "lane": self.lane
+        }
+
+        line_feature = Feature(geometry=multi_lines, properties=self.get_cause(line_prop))
+        raw_point_feature = Feature(geometry=raw_points, properties=self.get_cause(line_prop))
+
+        return line_feature, raw_point_feature
+
+
 def gpst_to_utc(gpst_seconds: pd.Series) -> int:
     # GPST = TAI - 19s (constant)
     t_tai = time.Time(gpst_seconds + 19, format='unix_tai', scale='tai')
@@ -24,7 +100,7 @@ def gpst_leapseconds(gpst_sec: pd.Series) -> int:
     t = time.Time(gpst_sec.iloc[0], format="unix", scale="tai")
     gpst_utc_ls = (t.unix_tai - t.unix) - 19
     t_corrected = time.Time(t.unix - gpst_utc_ls, format='unix')
-    print(f'GPST to UTC offset { gpst_utc_ls }')
+    print(f'GPST to UTC offset {gpst_utc_ls}')
     print(f'TAI from GPST {t.iso}')
     print(f'UTC corrected from GPST {t_corrected.iso}')
     t_final = time.Time(gpst_sec - gpst_utc_ls, format='unix')
@@ -53,3 +129,26 @@ def read_interaction(path: str) -> pd.DataFrame:
     interactions = pd.read_csv(path, parse_dates=['UtcDateTime'])
     interactions['UTC_Seconds'] = interactions['UtcDateTime'].astype('int64') // 1e9
     return interactions
+
+
+def meters_to_degrees(meters, latitude):
+    """
+    Approximation of meters based on positions latitude
+    :param meters:
+    :param latitude: current pos latitude
+    :return: Meters in degree
+    """
+    # Radius of the Earth in meters
+    radius = 6371000
+    # Calculate change in latitude
+    lat_change = meters / (radius * math.pi / 180)
+    # Calculate change in longitude
+    lon_change = meters / (radius * math.pi / 180 * math.cos(latitude * math.pi / 180))
+    #print(lat_change, lon_change)
+    return lat_change, lon_change
+
+
+def calc_parallel_c(a, b, distance):
+    ab = shp.LineString([a, b])
+    parallel = ab.offset_curve(distance=distance,quad_segs=32)
+    return parallel.coords[0], parallel.coords[1]
