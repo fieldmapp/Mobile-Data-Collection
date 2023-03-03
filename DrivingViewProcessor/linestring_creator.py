abbfebbdff492fc619879b63bee5b6298f402002
EricKrg1 <eric.krueger@dlr.de>
2023-03-03 16:09:05 +0100

    refactored to use geopandas, extended class model, added execution config

--- DrivingViewProcessor/linestring_creator.py
+++ DrivingViewProcessor/linestring_creator.py
@@ -1,11 +1,8 @@
-import geojson
 import pandas as pd
 import argparse
 import geopandas as gpd
-from utils import read_ublox_pos, read_interaction, add_time_col, gpst_leapseconds, LaneSegment, LaneDetail
-from typing import Dict, List
-from geojson import FeatureCollection
-from itertools import chain
+import shapely as shp
+from utils import read_ublox_pos, read_interaction, add_time_col, gpst_leapseconds, Field
 
 parser = argparse.ArgumentParser(description='Read and Process interaction und ublox data.')
 parser.add_argument("--pos", type=str, nargs=1)
@@ -15,59 +12,63 @@ parser.add_argument("--filename", type=str, nargs=1)
 _merge_rcol = "UTC_corrected"
 _merge_lcol = "UTC_Seconds"
 _position_time = "GPST_Seconds"
+# https://epsg.io/3043
+_base_epsg = 4326
+_calc_epsg = 3043
+
 _lane_config = {
-    1: 5,
-    2: 10,
-    3: 15,
-    4: -5,
-    5: -10,
-    6: -15,
+    1: 6,
+    2: 12,
+    3: 18,
+    4: -6,
+    5: -12,
+    6: -18,
+}
+_lane_width = 6
+
+
+def write_f(field: Field,
+            file_prefix: str,
+            output_name: str,
+            no_shift: bool,
+            geom_type: shp.Geometry):
+    with open(f"{file_prefix}_{output_name}", 'w') as f:
+        out = field.to_gpd(no_shift=no_shift, geom_type=geom_type)
+        f.write(out.to_json())
+
+
+def prepare_and_merge(pos_df: pd.DataFrame, int_df: pd.DataFrame, ) -> gpd.GeoDataFrame:
+    add_time_col(time_func=gpst_leapseconds,
+                 df=pos_df,
+                 col_name=_merge_rcol,
+                 base_col=_position_time)
+    interactions_df = int_df.sort_values(by=_merge_lcol)  # only needed for the test data set
+    # merge interactions_df and position
+    interactions_df = pd.merge_asof(interactions_df, pos_df, left_on=_merge_lcol, right_on=_merge_rcol)
+    geometry = gpd.points_from_xy(interactions_df["long_deg"], interactions_df['lat_deg'], crs=f"EPSG:{_base_epsg}")
+    geometry = geometry.to_crs(crs=f"EPSG:{_calc_epsg}")
+    return gpd.GeoDataFrame(data=interactions_df, geometry=geometry)
+
+
+_exec_config = {
+    "drive_line": lambda field, f_name: write_f(field=field, file_prefix="drive_line", no_shift=True, geom_type=shp.LineString, output_name=f_name),
+    "drive_points": lambda field, f_name: write_f(field=field, file_prefix="drive_points", no_shift=True, geom_type=shp.Point, output_name=f_name),
+    "drive_polygon": lambda field, f_name: write_f(field=field, file_prefix="drive_polygon", no_shift=True, geom_type=shp.Polygon, output_name=f_name),
+    "lane_polygon": lambda field, f_name: write_f(field=field, file_prefix="polygon", no_shift=False, geom_type=shp.Polygon, output_name=f_name),
+    "lane_line": lambda field, f_name: write_f(field=field, file_prefix="line", no_shift=False, geom_type=shp.LineString, output_name=f_name),
+    "lane_points": lambda field, f_name: write_f(field=field, file_prefix="points", no_shift=False, geom_type=shp.Point, output_name=f_name),
 }
 
 
-def lanes_from_df(df: gpd.GeoDataFrame, lane_config: dict) -> dict:
-    all_lanes: Dict[int, List[LaneSegment]] = dict()
-    for k, v in lane_config.items():
-        interactions_filtered: pd.DataFrame = df.loc[df["LaneIndex"] == k]
-        segment = None
-        lanes: [LaneSegment] = []
-        for interaction in interactions_filtered.itertuples(False):
-            detail = LaneDetail.from_pd(interaction)
-            if detail.ltype == 'open':
-                segment = LaneSegment(lane_index=interaction.LaneIndex, start=detail.utc_time)
-                segment.actions = [detail]
-                lanes.append(segment)
-            elif detail.ltype == 'close':
-                segment.end = detail.utc_time
-                segment.actions.append(detail)
-            else:
-                segment.actions.append(detail)
-        all_lanes[k] = lanes
-    return all_lanes
-
-
-def main(positions_df: pd.DataFrame, interactions_df: pd.DataFrame, file_name: str):
-    # add time columns, atm. both ways of calc.
-    add_time_col(time_func=gpst_leapseconds, df=positions_df, col_name=_merge_rcol, base_col=_position_time)
-    interactions_df = interactions_df.sort_values(by=_merge_lcol)  # only needed for the test data set
-    # merge interactions_df and position
-    interactions_df = pd.merge_asof(interactions_df, positions_df, left_on=_merge_lcol, right_on=_merge_rcol)
-    # is the geopandas df really nes. here?
-    geometry = gpd.points_from_xy(interactions_df["lat_deg"], interactions_df['long_deg'], crs="EPSG:4326")
-    interactions_df_gdf = gpd.GeoDataFrame(data=interactions_df, geometry=geometry)
+def main(positions_df: pd.DataFrame, interactions_df: pd.DataFrame, f_name: str, exec_list: [str]):
+    merged = prepare_and_merge(positions_df, interactions_df)
     # collect all lanes
-    result_lanes = lanes_from_df(interactions_df_gdf, _lane_config)
-    # lanes to geojson
-    features = [[e.to_geojson_feature(_lane_config) for e in v] for k, v in result_lanes.items()]
-    feature_list = list(chain.from_iterable(features))
-    f_list = [e for l in feature_list for e in l]  # unpack tuples
-    collection = FeatureCollection(features=f_list)
-
-    # write to file
-    out = geojson.dumps(collection, default=lambda x: x.__dict__)
-    with open(file_name, 'w') as f:
-        f.write(out)
-    print("done")
+    field = Field.from_gpd(df=merged,
+                           target_epsg=_base_epsg,
+                           lane_config=_lane_config,
+                           lane_width=_lane_width)
+    for e in exec_list:
+        _exec_config[e](field, f_name)
 
 
 if __name__ == "__main__":
@@ -75,5 +76,4 @@ if __name__ == "__main__":
     positions = read_ublox_pos(args.pos[0])
     interactions = read_interaction(args.ilog[0])
     file_name = args.filename[0]
-    main(positions, interactions, file_name)
-
+    main(positions, interactions, file_name, ["lane_polygon"])
--- DrivingViewProcessor/utils.py
+++ DrivingViewProcessor/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from astropy import time
-from geojson import Feature, MultiPoint, MultiLineString, LineString
+from typing import Dict
+import geopandas as gpd
 import math
 import shapely.geometry as shp
 
@@ -24,58 +25,170 @@ class LaneDetail:
 
 
 class LaneSegment:
-    start: int
-    end: int
-    actions: [LaneDetail]
+    start: LaneDetail
+    end: LaneDetail
     lane: int
+    cause: LaneDetail = None
+    damage: LaneDetail = None
+    width: float
 
-    def __init__(self, lane_index: int, start: int):
+    def __init__(self, lane_index: int, start: LaneDetail, width: float):
         self.lane = lane_index
         self.start = start
-        self.actions = []
+        self.width = width
 
-    def to_props(self):
-        res = self.__dict__
+    def add_lane_detail(self, detail: LaneDetail):
+        if detail.ltype == "close":
+            self.end = detail
+        elif detail.ltype.startswith("damage="):
+            self.damage = detail
+        elif detail.ltype.startswith("cause="):
+            self.damage = detail
+
+    def is_valid(self) -> bool:
+        # at least damage or cause have to be filled so that a lane detail is valid
+        return self.damage is not None or self.cause is not None
+
+    def to_dict(self, **kwargs) -> dict:
+        shift = kwargs.get("shift_by", None)
+        geom_type = kwargs.get("geom_type", shp.Point)
+        geom = self.get_open_close_as_points()
+
+        if shift is not None:
+            geom = self.get_shifted_segment(shift_by=shift)
+
+        if geom_type is shp.Point or geom_type is shp.MultiPoint:
+            geom = geom.boundary if type(geom) is shp.LineString else shp.MultiPoint(geom)
+        if geom_type is shp.LineString:
+            geom = shp.LineString(geom)
+        if geom_type is shp.Polygon:
+            geom = geom if type(geom) is shp.LineString else shp.LineString(geom)
+            geom = construct_parallel_polygon((geom.xy[0][0], geom.xy[1][0]),
+                                              (geom.xy[0][1], geom.xy[1][1]), self.width)
+        res = {
+            "start": self.start.utc_time,
+            "lane": self.lane,
+            "geometry": geom
+        }
         res = self.get_cause(res)
         return res
 
     def get_cause(self, res: dict):
-        for i in self.actions:
-            if "=" in i.ltype:
-                split = i.ltype.split("=")
-                res[split[0]] = split[1]
+        if self.cause is not None:
+            cause = self.cause.ltype.split("=")
+            res[cause[0]] = cause[1]
+        if self.damage is not None:
+            dmg = self.damage.ltype.split("=")
+            res[dmg[0]] = dmg[1]
         return res
 
-    def to_geojson_feature(self, lane_config: dict) -> Feature:
-        """
-        :param lane_config: Configuration of the field lanes: number and width
-        :return: GeoJson Feature with the calculated parallel Lines representing the FieldLanes
-                as well as a Multipoint feature representing the GPS-Track
-        """
-        open_node = next(x for x in self.actions if x.ltype == "open")
-        close_node = next(x for x in self.actions if x.ltype == "close")
-
-        if open_node is None:
+    def get_open_close(self):
+        if self.start is None:
             raise ValueError(f"No open node found for {self}")
-        if close_node is None:
+        if self.end is None:
             raise ValueError(f"No close node found for {self}")
+        return self.start, self.end
 
-        a, b = calc_parallel_c((open_node.lon, open_node.lat), (close_node.lon, close_node.lat),
-                               meters_to_degrees(lane_config[self.lane], (open_node.lat+close_node.lat)/2)[1])
-        # constructed parallel shifted line
-        line_shifted = LineString([a, b], precision=15)
-        raw_points = MultiPoint([(open_node.lon, open_node.lat), (close_node.lon, close_node.lat)], precision=15)
+    def get_open_close_as_points(self) -> tuple:
+        o, c = self.get_open_close()
+        return shp.Point(o.lat, o.lon), shp.Point(c.lat, c.lon)
 
-        multi_lines = MultiLineString([line_shifted], precision=15)
-        line_prop = {
+    def get_line_prop(self) -> dict:
+        return {
             "start": self.start,
             "lane": self.lane
         }
 
-        line_feature = Feature(geometry=multi_lines, properties=self.get_cause(line_prop))
-        raw_point_feature = Feature(geometry=raw_points, properties=self.get_cause(line_prop))
+    def get_shifted_segment(self, shift_by: float) -> shp.LineString:
+        open_node, close_node = self.get_open_close()
+        a, b = calc_parallel_c((open_node.lat, open_node.lon), (close_node.lat, close_node.lon), shift_by)
+        return shp.LineString([a, b])
 
-        return line_feature, raw_point_feature
+
+class Lane:
+    lane_nr: int
+    segments: [LaneSegment]
+    lane_shift: float
+    lane_width: float
+    crs: str
+
+    def __init__(self, lane_nr: int, segments: [LaneSegment], shift: float, width: float ,crs: str):
+        # filter before adding segements to lane
+        segs = [i for i in segments if i.is_valid()]
+        self.lane_nr = lane_nr
+        self.segments = segs
+        self.lane_shift = shift
+        self.crs = crs
+        self.lane_width = width
+
+    def to_gpd(self, no_shift: bool, **kwargs) -> gpd.GeoDataFrame:
+        geom_type = kwargs.get("geom_type", shp.Point)
+        i: LaneSegment
+        rows = [i.to_dict(shift_by=None if no_shift else self.lane_shift,
+                          geom_type=geom_type) for i in self.segments]
+        df = gpd.GeoDataFrame(rows, crs=self.crs)
+        return df
+
+
+class Field:
+    lanes: dict
+    input_epsg: int
+    target_epsg: int
+    lane_config: dict
+    lane_width: float
+
+    def __init__(self, all_lanes, input_epsg: int, target_epsg: int, lane_config: dict, lane_width: float):
+        self.lanes = all_lanes
+        self.lane_config = lane_config
+        self.input_epsg = input_epsg
+        self.target_epsg = target_epsg
+        self.lane_width = lane_width
+
+    def to_gpd(self, **kwargs) -> gpd.GeoDataFrame:
+        # should the output geom be the shifted representation of the recorded lane
+        # if true output geom is the start and end point of a recorded lane
+        # if false output geom is the linestring of the recorded lane shifted by
+        # its defined distance from the driving lane
+        no_shift = kwargs.get("no_shift", True)
+        geom_type = kwargs.get("geom_type", shp.Point)
+        v: Lane
+        res: gpd.GeoDataFrame
+        res = pd.concat([v.to_gpd(no_shift=no_shift, geom_type=geom_type) for k, v in self.lanes.items()])
+        if self.input_epsg != self.target_epsg:
+            res = res.to_crs(epsg=self.target_epsg)
+        return res
+
+    @classmethod
+    def from_gpd(cls, df: gpd.GeoDataFrame, target_epsg: int, lane_config: dict, lane_width: float):
+        all_l: Dict[int, Lane] = dict()
+        for k, v in lane_config.items():
+            interactions_filtered: pd.DataFrame = df.loc[df["LaneIndex"] == k]
+            segment = None
+            lanes: [LaneSegment] = []
+            for interaction in interactions_filtered.itertuples(False):
+                detail = LaneDetail.from_pd(interaction)
+                if detail.ltype == 'open':
+                    segment = LaneSegment(lane_index=interaction.LaneIndex,
+                                          start=detail,
+                                          width=lane_width)
+                    lanes.append(segment)
+                else:
+                    try:
+                        segment.add_lane_detail(detail)
+                    except Exception as e:
+                        # if segment was never opened
+                        print(f"Error creating segment {e}")
+            all_l[k] = Lane(lane_nr=k,
+                            segments=lanes,
+                            shift=lane_config[k],
+                            width=lane_width,
+                            crs=df.crs)
+        field = Field(all_lanes=all_l,
+                      lane_config=lane_config,
+                      target_epsg=target_epsg,
+                      input_epsg=df.crs,
+                      lane_width=lane_width)
+        return field
 
 
 def gpst_to_utc(gpst_seconds: pd.Series) -> int:
@@ -144,11 +257,17 @@ def meters_to_degrees(meters, latitude):
     lat_change = meters / (radius * math.pi / 180)
     # Calculate change in longitude
     lon_change = meters / (radius * math.pi / 180 * math.cos(latitude * math.pi / 180))
-    #print(lat_change, lon_change)
+    print(lat_change, lon_change)
     return lat_change, lon_change
 
 
-def calc_parallel_c(a, b, distance):
+def calc_parallel_c(a: tuple, b: tuple, distance: float):
     ab = shp.LineString([a, b])
-    parallel = ab.offset_curve(distance=distance,quad_segs=32)
+    parallel = ab.offset_curve(distance=distance, quad_segs=32)
     return parallel.coords[0], parallel.coords[1]
+
+
+def construct_parallel_polygon(a: tuple, b: tuple, height: float) -> shp.Polygon:
+    c, d = calc_parallel_c(a, b, -1 * height)
+    polygon = shp.Polygon([a, c, d, b])
+    return polygon
